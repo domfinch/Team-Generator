2b37fcf113e2ac444f3e38e96b98a477d3f28d7d
domfinch <finch.163@buckeyemail.osu.edu>
2022-06-21 16:39:45 -0400

    start index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const Engineer = require("./lib/Engineer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+//packages
+const fs = require("fs");
+const inquirer = require("inquirer");
+//Questions for Team Manger Info
